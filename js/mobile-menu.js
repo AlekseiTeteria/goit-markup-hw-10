67360b2c5644a135b15969f3552e4a3ba9fd4cbb
ProkoptsovD <dimka.prokoptsov@gmail.com>
2021-12-22 13:26:25 +0200

    unables body scroll while mobile menu is opened
    also fixes modal height

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,12 @@
     const menuBtnRef = document.querySelector("[data-burger-btn]");
     const mobileMenuRef = document.querySelector("[data-menu-container]");
 
+    const body = document.getElementsByTagName("body")[0];
+
     menuBtnRef.addEventListener("click", () => {
+
+        body.classList.toggle("modal-open");
+
         const expanded =
             menuBtnRef.getAttribute("aria-expanded") === "true" || "false";
     
@@ -13,12 +18,12 @@
 
     });
 
-     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-opened');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
+//      window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-opened');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
 })();
 
 // (() => {
